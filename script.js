5da041192c94dbb6d5e9ca52e9ffb1654139225b
Nate Martinez <natemartinez@Nates-iMac.local>
2020-11-04 18:53:22 -0700

    Added functionality that allows the user to add a todo by hitting enter.

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
 const todoInput = document.getElementById('todoInput')
 const addButton = document.getElementById('add')
-const editButton = document.getElementById('edit')
-const deleteButton = document.getElementById('delete')
 const todoContainer = document.getElementById('todo-container')
 
-// window.addEventListener('keypress', e => getInputText(e))
-addButton.addEventListener('click', e => getInputText(e, true))
+const todos = []
 
-function getInputText() {
+window.addEventListener('keypress', e => getInputText(e))
+addButton.addEventListener('click', e => getInputText(e))
+// if (todos.length > 0) {
+//   deleteButton.addEventListener('click', e => deleteTodo(e))
+//   editButton.addEventListener('click', e => editTodo(e))
+// }
+
+function getInputText(e) {
   const newTodoText = todoInput.value
-  addTodo(newTodoText)
-  todoInput.value = ''
+  if (e.code === 'Enter' || e.target.classList[0] === 'fas') {
+    addTodo(newTodoText)
+    todoInput.value = ''
+  }
 }
 
 function addTodo(todoText) {
-  let todo = document.createElement('div')
-  todo.classList.add('todo')
-  todo.innerHTML = ` <input value="${todoText}" id="todo-text" type="text" autocomplete="off" readonly>
-  <button id="edit" type="button">Edit</button>
-  <button id="delete" type="button">Delete</button>`
-  todoContainer.appendChild(todo)
+  todos.push(todoText)
+  showTodos()
+}
+
+function showTodos() {
+  if (todos.length > 0) {
+    todoContainer.innerHTML = ''
+    todos.forEach(todoText => {
+      let todo = document.createElement('div')
+      todo.classList.add('todo')
+      todo.innerHTML = ` <input value="${todoText}" class="todo-text" type="text" autocomplete="off" readonly>
+  <button class="edit" type="button">Edit</button>
+  <button class="delete" type="button">Delete</button>`
+      todoContainer.appendChild(todo)
+    })
+  }
+}
+
+function deleteTodo(e) {
+  console.log(e.target.classList)
+}
+
+function editTodo(params) {
+
 }
