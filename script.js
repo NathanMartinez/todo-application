5f7f745219c6153f23bf70b2eebc4346c2193bcc
Nate Martinez <natemartinez@Nates-iMac.local>
2020-11-04 20:14:50 -0700

    Added completion, edit, and deletion functionality.
    Minor Bug Fixes regarding adding of todos, as well as adding code to remove and edit the todos array as well the page.

--- script.js
+++ script.js
@@ -6,16 +6,14 @@ const todos = []
 
 window.addEventListener('keypress', e => getInputText(e))
 addButton.addEventListener('click', e => getInputText(e))
-// if (todos.length > 0) {
-//   deleteButton.addEventListener('click', e => deleteTodo(e))
-//   editButton.addEventListener('click', e => editTodo(e))
-// }
 
 function getInputText(e) {
   const newTodoText = todoInput.value
-  if (e.code === 'Enter' || e.target.classList[0] === 'fas') {
-    addTodo(newTodoText)
-    todoInput.value = ''
+  if (newTodoText !== '') {
+    if (e.code === 'Enter' || e.target.id === 'add') {
+      addTodo(newTodoText)
+      todoInput.value = ''
+    }
   }
 }
 
@@ -27,21 +25,40 @@ function addTodo(todoText) {
 function showTodos() {
   if (todos.length > 0) {
     todoContainer.innerHTML = ''
-    todos.forEach(todoText => {
+    todos.forEach((todoText, i) => {
       let todo = document.createElement('div')
+      todo.id = i
       todo.classList.add('todo')
       todo.innerHTML = ` <input value="${todoText}" class="todo-text" type="text" autocomplete="off" readonly>
-  <button class="edit" type="button">Edit</button>
-  <button class="delete" type="button">Delete</button>`
+      <button class="complete" type="button">Complete</button>
+      <button class="edit" type="button">Edit</button>
+      <button class="delete" type="button">Delete</button>`
       todoContainer.appendChild(todo)
+      todo.addEventListener('click', e => setTodoStatus(e))
     })
   }
 }
 
-function deleteTodo(e) {
-  console.log(e.target.classList)
-}
+function setTodoStatus(e) {
+  const todoId = e.target.parentElement.id
+  const selectedButton = e.target.classList[0]
 
-function editTodo(params) {
-
+  switch (selectedButton) {
+    case 'complete':
+      e.target.parentElement.classList.toggle('completed')
+      break;
+    case 'edit':
+      let todoInput = e.target.parentElement.childNodes[1]
+      let updatedTodoText = todoInput.value
+      todoInput.readOnly = !todoInput.readOnly
+      todos[todoId] = updatedTodoText
+      todoInput.focus()
+      break;
+    case 'delete':
+      todos.splice(todoId, 1)
+      e.target.parentElement.remove()
+      break;
+    default:
+      break;
+  }
 }
